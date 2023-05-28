2a231f5e575d5be073598b0e6e22ed7762fd257f
19829984 <57331630+19829984@users.noreply.github.com>
2023-05-28 01:36:16 -0400

    Now use bmesh to create transpose target directly, and bmesh to create individual targets when applying transpose

--- multires_transpose.py
+++ multires_transpose.py
@@ -4,7 +4,7 @@ import bmesh
 import time
 import logging
 from .data_types import MeshDomain, MeshLayerType
-from .utils.bmesh_layer_ops import write_layer_data, read_layer_data
+from .utils.bmesh_utils import write_layer_data, read_layer_data, bmesh_join, bmesh_from_faces
 from .utils.bmesh_context import bmesh_from_obj
 
 ORIGINAL_OBJECT_NAME_LAYER = "original_object_name"
@@ -16,12 +16,13 @@ class LoggerOperator(bpy.types.Operator):
         self.logger = logging.getLogger(__name__ + "." + self.__class__.__name__)
 
 
-def set_multires_to_first_level(objects: Iterable[bpy.types.Object]):
+def set_multires_to_nth_level(objects: Iterable[bpy.types.Object], n: int) -> set[bpy.types.Object]:
     """
     set all selected object's multiresolution modifier's view subdivision level to the first level
 
     Args:
         objects (Iterable[bpy.types.Object]): Objects to change multires level on
+        n (int): Level to set multires to
 
     Returns:
         set[bpy.types.Object]: Objects that have had they multires level changed
@@ -31,84 +32,14 @@ def set_multires_to_first_level(objects: Iterable[bpy.types.Object]):
         if obj.type == "MESH":
             for mod in obj.modifiers:
                 if mod.type == "MULTIRES":
-                    mod.levels = 1
+                    mod.levels = n
                     changed_objs.add(obj)
                     break
 
     return changed_objs
 
 
-def duplicate(obj: bpy.types.Object, data: bool = True, actions: bool = True, collection: bpy.types.Collection = None):
-    """
-    Duplicate an object, including its data and animation data
-    From: https://b3d.interplanety.org/en/making-a-copy-of-an-object-using-the-blender-python-api/
-
-    Args:
-        obj (bpy.types.Object): object to duplicate
-        data (bool, optional): duplicate data. Defaults to True.
-        actions (bool, optional): duplicate actions. Defaults to True.
-        collection (bpy.types.Collection, optional): collection to link to. Defaults to None.
-
-    Returns:
-        bpy.types.Object: duplicated object
-    """
-    obj_copy = obj.copy()
-    if data:
-        obj_copy.data = obj_copy.data.copy()
-    if actions and obj_copy.animation_data:
-        obj_copy.animation_data.action = obj_copy.animation_data.action.copy()
-    collection.objects.link(obj_copy)
-    return obj_copy
-
-
-def duplicate_mesh_objects(context: bpy.types.Context, objects: Iterable[bpy.types.Object], suffix: str = "_copy"):
-    """
-    Duplicate objects in the scene
-
-    Args:
-        context (bpy.types.Context): Context to get depsgraph from
-        objects (Iterable[bpy.types.Object]): Objects to duplicate
-        suffix (str, optional): Suffix to append to the name of the duplicated objects. Defaults to "_copy".
-
-    Returns:
-        set[bpy.types.Object]: Objects that have been duplicated
-    """
-    new_objs = set()
-    for obj in objects:
-        if obj.type == "MESH":
-            new_obj = duplicate(obj=obj, collection=context.collection)
-            # Set name of new object to be the same as the old object with a suffix _copy
-            new_obj.name = obj.name + suffix
-
-            new_objs.add(new_obj)
-
-    return new_objs
-
-
-def record_required_data(context: bpy.types.Context, objects: Iterable[bpy.types.Object], original_objects: Iterable[bpy.types.Object]):
-    """
-    Record the required data of the given objects
-
-    Args:
-        context (bpy.types.Context): Context to get depsgraph from
-        objects (Iterable[bpy.types.Object]): Objects to record required data on
-        original_objects (Iterable[bpy.types.Object]): Original objects to record required data from
-    """
-    for object, original_object in zip(objects, original_objects):
-        with bmesh_from_obj(object) as bm:
-            # Record the original object names in the new object's face layer
-            write_layer_data(bm, MeshDomain.FACES, MeshLayerType.STRING, ORIGINAL_OBJECT_NAME_LAYER, [original_object.name for f in bm.faces])
-
-            # Record the original vertex indices in the new object's vertex layer
-            depsgraph = context.evaluated_depsgraph_get()
-            multires_mesh = depsgraph.objects[original_object.name].data
-            write_layer_data(bm, MeshDomain.VERTS, MeshLayerType.INT, ORIGINAL_VERTEX_INDEX_LAYER, [v.index for v in multires_mesh.vertices])
-
-            # Record the original object's origin in the new object's vertex layer
-            write_layer_data(bm, MeshDomain.VERTS, MeshLayerType.FLOAT_VECTOR, "original_object_origin", [original_object.location for v in bm.verts])
-
-
-def restore_vertex_index(bm: bmesh.types.BMesh):
+def restore_vertex_index(bm: bmesh.types.BMesh) -> None:
     """
     Restore the vertex indices of the given object to the original vertex indices
 
@@ -121,68 +52,88 @@ def restore_vertex_index(bm: bmesh.types.BMesh):
     bm.verts.sort()
 
 
-def restore_object_origin(bm: bmesh.types.BMesh):
+def split_meshes_by_original_name(object: bpy.types.Object) -> List[bpy.types.Object]:
     """
-    Restore the object origin of the given object to the original object origin
+    Split the given object into multiple objects based on the original object name recorded in the mesh's face layer
 
     Args:
-        object (bpy.types.Object): Object to restore object origin on
+        object (bpy.types.Object): Object to split
 
     Returns:
-        Vector: Original object origin
+        List[bpy.types.Object]: List of split objects
     """
-    original_object_origins = read_layer_data(bm, MeshDomain.VERTS, MeshLayerType.FLOAT_VECTOR, "original_object_origin", uniform=True)
-    bmesh.ops.translate(bm, verts=bm.verts, vec=-original_object_origins)
-    return original_object_origins
+    split_objects = []
 
-
-def restore_recorded_data(object: bpy.types.Object):
-    """
-    Restore the recorded data on the given object
-
-    Args:
-        object (bpy.types.Object): Object to restore recorded data on
-    """
     with bmesh_from_obj(object) as bm:
-        restore_vertex_index(bm)
-        original_origin = restore_object_origin(bm)
-    object.location += original_origin
+        original_obj_names = read_layer_data(bm, MeshDomain.FACES, MeshLayerType.STRING, ORIGINAL_OBJECT_NAME_LAYER, uniform=False)
+        tranpose_map = {name: [] for name in set(original_obj_names)}
+
+        # Create a map of original object names to faces
+        for face, name in zip(bm.faces, original_obj_names):
+            tranpose_map[name].append(face)
+
+        for obj_name, faces in tranpose_map.items():
+            face_index_min = min([f.index for f in faces])
+            face_index_max = max([f.index for f in faces])
+
+            # Create a new bmesh from the faces associated with the original object
+            d_bm = bmesh_from_faces(bm, bm.faces[face_index_min:face_index_max + 1])
+            temp_mesh = bpy.data.meshes.new(name=f"{obj_name}_tgt")
+            d_bm.to_mesh(temp_mesh)
+            d_bm.free()
+
+            # Create object from mesh and link it
+            tmp_obj = bpy.data.objects.new(name=f"{obj_name}_Target", object_data=temp_mesh)
+            bpy.context.collection.objects.link(tmp_obj)
+            split_objects.append(tmp_obj)
+
+    return split_objects
 
 
-def merge_objects(objects: List[bpy.types.Object]):
+def copy_multires_objs_to_new_mesh(context: bpy.types.Context, objects: Iterable[bpy.types.Object], level: int = 1) -> bpy.types.Object:
     """
-    Merge the given objects into a single object
+    Copy all objects to a new mesh at the given multires level, if they have a multires modifier.
 
     Args:
-        objects (List[bpy.types.Object]): Objects to merge
+        context (bpy.types.Context): context
+        objects (Iterable[bpy.types.Object]): objects to copy from
+        level (int, optional): Multires subdivision level. Defaults to 1.
 
     Returns:
-        bpy.types.Object: Merged object
+        bpy.types.Object: merged object
     """
-    if len(objects) < 2:
-        return None
+    # Create new mesh and object, then link it
+    transpose_target_mesh = bpy.data.meshes.new(name="Multires_Transpose_Target")
 
-    with bpy.context.temp_override(active_object=objects[0], selected_editable_objects=objects):
-        # Merge objects
-        bpy.ops.object.join()
+    set_multires_to_nth_level(objects, level)
+    depsgraph = context.evaluated_depsgraph_get()
 
-        return bpy.context.active_object
+    bms = []
 
-
-def apply_modifiers(objects: Iterable[bpy.types.Object]):
-    """
-    Apply all modifiers on the given objects
-
-    Args:
-        objects (Iterable[bpy.types.Object]): Objects to apply modifiers on
-    """
     for object in objects:
-        with bpy.context.temp_override(object=object):
-            for mod in object.modifiers:
-                bpy.ops.object.modifier_apply(modifier=mod.name)
+        bm = bmesh.new()
+        bm.from_mesh(depsgraph.objects[object.name].data)
+        # Apply transformations to the mesh
+        bmesh.ops.transform(bm, verts=bm.verts, matrix=object.matrix_world)
 
+        # Record the original object names in the new object's face layer
+        write_layer_data(bm, MeshDomain.FACES, MeshLayerType.STRING, ORIGINAL_OBJECT_NAME_LAYER, [object.name for f in bm.faces])
 
-# def separate_mesh_by_original_object_name(bm: bmesh.types.BMesh):
+        # Record the original vertex indices in the new object's vertex layer
+        write_layer_data(bm, MeshDomain.VERTS, MeshLayerType.INT, ORIGINAL_VERTEX_INDEX_LAYER, [v.index for v in bm.verts])
+
+        # Record the original object's origin in the new object's vertex layer
+        write_layer_data(bm, MeshDomain.VERTS, MeshLayerType.FLOAT_VECTOR, "original_object_origin", [object.location for v in bm.verts])
+        bms.append(bm)
+
+    final_bm = bmesh.new()
+    final_bm = bmesh_join(bms)
+    final_bm.to_mesh(transpose_target_mesh)
+    final_bm.free()
+
+    transpose_target_obj = bpy.data.objects.new(name="Multires_Transpose_Target", object_data=transpose_target_mesh)
+    context.collection.objects.link(transpose_target_obj)
+    return transpose_target_obj
 
 
 class MULTIRES_TRANSPOSE_OT_create_transpose_target(LoggerOperator):
@@ -193,18 +144,13 @@ class MULTIRES_TRANSPOSE_OT_create_transpose_target(LoggerOperator):
     def execute(self, context):
         start_time = time.time()
 
-        objs = set_multires_to_first_level(context.selected_objects)
-        new_objs = duplicate_mesh_objects(context, objs)
-        set_multires_to_first_level(new_objs)
-        apply_modifiers(new_objs)
-        record_required_data(context, new_objs, objs)
-        merged_obj = merge_objects(list(new_objs))
-        merged_obj.name = "Multires_Transpose_Target"
+        transpose_target = copy_multires_objs_to_new_mesh(context, context.selected_objects)
+        transpose_target.name = "Multires_Transpose_Target"
 
-        for obj in objs:
+        for obj in context.selected_objects:
             obj.select_set(False)
-        context.view_layer.objects.active = merged_obj
-        merged_obj.select_set(True)
+        context.view_layer.objects.active = transpose_target
+        transpose_target.select_set(True)
 
         self.logger.info(f"Time taken to create Tranpose Target: {time.time() - start_time}")
         return {"FINISHED"}
@@ -221,46 +167,37 @@ class MULTIRES_TRANPOSE_OT_apply_transpose_target(LoggerOperator):
         if active_obj.name != "Multires_Transpose_Target":
             self.report({'WARNING'}, "Selected object does not have name 'Multires_Transpose_Target', operation may fail")
 
-        # TODO: Separate objects based on original object name
-        bpy.ops.object.editmode_toggle()
-        bpy.ops.mesh.separate(type="LOOSE")
-        bpy.ops.object.editmode_toggle()
-
-        separated_objects = context.selected_objects
-        for object in separated_objects:
+        tranpose_targets = split_meshes_by_original_name(active_obj)
+        for object in tranpose_targets:
             original_obj_name = ""
             with bmesh_from_obj(object) as bm:
-                original_obj_name = read_layer_data(bm, MeshDomain.FACES, MeshLayerType.STRING, ORIGINAL_OBJECT_NAME_LAYER, uniform=True)
-            if not original_obj_name:
+                original_obj_name = object.name[:-len("_Target")]
+            if original_obj_name not in bpy.data.objects:
                 self.logger.warn(f"Object {object.name} does not have original object name recorded, skipping")
                 continue
+
             original_obj = bpy.data.objects[original_obj_name]
+            with bmesh_from_obj(object) as bm:
+                restore_vertex_index(bm)
+                bmesh.ops.transform(bm, verts=bm.verts, matrix=original_obj.matrix_world.inverted())
 
-            restore_recorded_data(object)
-            selected_objs = (original_obj, object)
-            with bpy.context.temp_override(object=original_obj, selected_editable_objects=selected_objs):
-                # TODO: Auto iteration count
-                for _ in range(iters):
-                    bpy.ops.object.multires_reshape(modifier="Multires")
+                bm.to_mesh(object.data)
 
-        # Join objects back together
-        merged_obj = merge_objects(tuple(separated_objects))
-        merged_obj.name = "Multires_Transpose_Target"
+                selected_objs = (original_obj, object)
+                with bpy.context.temp_override(object=original_obj, selected_editable_objects=selected_objs):
+                    # TODO: Auto iteration count
+                    for _ in range(iters):
+                        bpy.ops.object.multires_reshape(modifier="Multires")
+
+                bmesh.ops.transform(bm, verts=bm.verts, matrix=original_obj.matrix_world)
+
+        # Remove all transpose targets
+        for obj in tranpose_targets:
+            bpy.data.objects.remove(obj)
 
         return {'FINISHED'}
 
 
-class TestOperator(bpy.types.Operator):
-    bl_idname = "multires_transpose.test_operator"
-    bl_label = "Test Operator"
-
-    def execute(self, context):
-        obj = context.active_object
-        restore_vertex_index(obj)
-
-        return {"FINISHED"}
-
-
-classes = (MULTIRES_TRANSPOSE_OT_create_transpose_target, TestOperator, MULTIRES_TRANPOSE_OT_apply_transpose_target)
+classes = (MULTIRES_TRANSPOSE_OT_create_transpose_target, MULTIRES_TRANPOSE_OT_apply_transpose_target)
 
 register, unregister = bpy.utils.register_classes_factory(classes)
---
+++ utils/bmesh_utils.py
@@ -0,0 +1,210 @@
+import bmesh
+from ..data_types import MeshDomain, MeshLayerType
+from typing import Iterable, Any
+import operator
+
+ALL_DOMAINS = {'faces', 'edges', 'verts', 'loops'}
+ALL_POSSIBLE_LAYERS = {'bevel_weight', 'int', 'paint_mask', 'float_color', 'string', 'freestyle', 'skin', 'float_vector', 'uv', 'shape', 'deform', 'crease', 'face_map', 'color', 'float'}
+GET_LAYER_FNS = [operator.attrgetter(f'{domain}.layers') for domain in ALL_DOMAINS]
+
+
+def resolve_domain_and_layer_type(bm: bmesh.types.BMesh, domain: MeshDomain, layer_type: MeshLayerType, layer_name: str) -> tuple[bmesh.types.BMElemSeq, Any]:
+    """
+    Resolve the domain and layer type to the corresponding bmesh domain and layer type
+
+    Args:
+        bm (bmesh.types.BMesh): bmesh object to resolve domain and layer type on
+        domain (MeshDomain): domain where the data is stored
+        layer_type (MeshLayerType): type of the data
+        layer_name (str): name of the data layer
+
+    Returns:
+        dom, layer: resolved domain and layer object
+    """
+    match domain:
+        case MeshDomain.FACES:
+            dom = bm.faces
+        case MeshDomain.LOOPS:
+            dom = bm.loops
+        case MeshDomain.EDGES:
+            dom = bm.edges
+        case MeshDomain.VERTS:
+            dom = bm.verts
+
+    match layer_type:
+        case MeshLayerType.STRING:
+            layer = dom.layers.string.get(layer_name, None)
+            if not layer:
+                layer = dom.layers.string.new(layer_name)
+        case MeshLayerType.INT:
+            layer = dom.layers.int.get(layer_name, None)
+            if not layer:
+                layer = dom.layers.int.new(layer_name)
+        case MeshLayerType.FLOAT:
+            layer = dom.layers.float.get(layer_name, None)
+            if not layer:
+                layer = dom.layers.float.new(layer_name)
+        case MeshLayerType.FLOAT_VECTOR:
+            layer = dom.layers.float_vector.get(layer_name, None)
+            if not layer:
+                layer = dom.layers.float_vector.new(layer_name)
+
+    return dom, layer
+
+
+def write_layer_data(bm: bmesh.types.BMesh, domain: MeshDomain, layer_type: MeshLayerType, layer_name: str, data: Iterable[Any], start_index: int = 0) -> None:
+    """
+    Write custom data to a mesh's layer at one of its types, create the layer if it doesn't exist.
+
+    Args:
+        bm (bmesh.types.BMesh): bmesh object to write to
+        domain (MeshDomain): Domain to read from
+        layer_type (MeshLayerType): Layer type to read from
+        layer_name (str): Name of the layer to write to
+        data (Iterable[Any]): Data to write
+        start_index (int, optional): Index to start writing data from. Defaults to 0.
+    """
+    dom, layer = resolve_domain_and_layer_type(bm, domain, layer_type, layer_name)
+
+    # Check if data contains string data and encode them to bytes
+    if data and isinstance(data[0], str):
+        data = [bytes(d, "utf-8") for d in data]
+
+    for dat, dom_elemnt in zip(data, dom[start_index:len(data)]):
+        dom_elemnt[layer] = dat
+
+
+def read_layer_data(bm: bmesh.types.BMesh, domain: MeshDomain, layer_type: MeshLayerType, layer_name: str, uniform: bool = False, start_index: int = 0, size: int = None) -> Iterable[Any]:
+    """
+    Read custom data from a mesh's layer at one of its types, create the layer if it doesn't exist.
+
+    Args:
+        bm (bmesh.types.BMesh): bmesh object to read from
+        domain (MeshDomain): Domain to read from
+        layer_type (MeshLayerType): Layer type to read from
+        layer_name (str): Name of the layer to read from
+        uniform (bool, optional): Whether the data is expected to be uniform. Defaults to False.
+        start_index (int, optional): Index to start reading data from. Defaults to 0.
+        size (int, optional): Number of data to read. Defaults to None.
+
+    Returns:
+        Iterable[Any]: Data read
+    """
+    dom, layer = resolve_domain_and_layer_type(bm, domain, layer_type, layer_name)
+
+    if uniform:
+        # Early exit on uniform data
+        for dom_elemnt in dom:
+            data = dom_elemnt[layer]
+            if data and isinstance(data, bytes):
+                return data.decode("utf-8")
+            return data
+
+    data = [dom_elemnt[layer] for dom_elemnt in dom[start_index:size]]
+
+    # Check if data contains string data and decode them to strings
+    if data and isinstance(data[0], bytes):
+        data = [d.decode("utf-8") for d in data]
+
+    return data
+
+
+def copy_all_layers(src_bmesh: bmesh.types.BMesh, dst_bmesh: bmesh.types.BMesh) -> None:
+    """
+    Copy all layers from src_bmesh to dst_bmesh
+
+    Args:
+        src_bmesh (bmesh.types.BMesh): bmesh to copy from
+        dst_bmesh (bmesh.types.BMesh): bmesh to copy to
+    """
+    for get_layers in GET_LAYER_FNS:
+        layers = get_layers(src_bmesh)  # equivalent to src_bmesh.{domain}.layers
+        layer_names = [available_layer for available_layer in dir(layers) if available_layer in ALL_POSSIBLE_LAYERS]
+
+        get_layer_attr_fns = [operator.attrgetter(layer) for layer in layer_names]
+        for get_layer_attr in get_layer_attr_fns:
+            attrs = get_layer_attr(layers)  # equivalent to src_bmesh.{domain}.layers.{layer}
+
+            # For loop filters out empty layers
+            for name, _ in attrs.items():
+                get_layer_attr(get_layers(dst_bmesh)).new(name)
+
+
+def bmesh_from_faces(src_bmesh: bmesh.types.BMesh, faces: Iterable[bmesh.types.BMFace]) -> bmesh.types.BMesh:
+    """
+    Create a new bmesh from a given sequence of faces
+
+    Args:
+        src_bmesh (bmesh.types.BMesh): source bmesh to copy from
+        faces (Iterable[bmesh.types.BMFace]): faces to copy
+
+    Returns:
+        bmesh.types.BMesh: new bmesh containing the given faces
+    """
+
+    dst_bmesh = bmesh.new()
+    copy_all_layers(src_bmesh, dst_bmesh)
+    min_vert_index = min([v.index for f in faces for v in f.verts])
+
+    # Copy vertices and assign correct indices
+    accessed_indices = set()
+    for face in faces:
+        for v in face.verts:
+            if v.index not in accessed_indices:
+                nv = dst_bmesh.verts.new(v.co, v)
+                nv.index = v.index - min_vert_index
+                accessed_indices.add(v.index)
+    dst_bmesh.verts.sort()
+    dst_bmesh.verts.index_update()
+    dst_bmesh.verts.ensure_lookup_table()
+
+    # Copy faces
+    for face in faces:
+        dst_bmesh.faces.new(tuple(dst_bmesh.verts[v.index - min_vert_index] for v in face.verts), face)
+    dst_bmesh.faces.index_update()
+    dst_bmesh.faces.sort()
+
+    return dst_bmesh
+
+
+def bmesh_join(list_of_bmeshes: Iterable[bmesh.types.BMesh], normal_update=False) -> bmesh.types.BMesh:
+    """ takes as input a list of bm references and outputs a single merged bmesh
+    allows an additional 'normal_update=True' to force _normal_ calculations.
+    Modified from https://blender.stackexchange.com/questions/50160/scripting-low-level-join-meshes-elements-hopefully-with-bmesh
+    """
+
+    bm = bmesh.new()
+    add_vert = bm.verts.new
+    add_face = bm.faces.new
+    add_edge = bm.edges.new
+
+    copy_all_layers(list_of_bmeshes[0], bm)
+
+    for bm_to_add in list_of_bmeshes:
+        offset = len(bm.verts)
+
+        for v in bm_to_add.verts:
+            add_vert(v.co, v)
+
+        bm.verts.index_update()
+        bm.verts.ensure_lookup_table()
+
+        if bm_to_add.faces:
+            for face in bm_to_add.faces:
+                add_face(tuple(bm.verts[i.index + offset] for i in face.verts), face)
+            bm.faces.index_update()
+
+        if bm_to_add.edges:
+            for edge in bm_to_add.edges:
+                edge_seq = tuple(bm.verts[i.index + offset] for i in edge.verts)
+                try:
+                    add_edge(edge_seq, edge)
+                except ValueError:
+                    # edge exists!
+                    pass
+            bm.edges.index_update()
+
+    if normal_update:
+        bm.normal_update()
+
+    return bm
